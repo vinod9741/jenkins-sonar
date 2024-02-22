bdfeeb041335a92c4d23d131388fa106cdbdd55d
vinod9741 <156701180+vinod9741@users.noreply.github.com>
2024-02-23 01:26:39 +0530

    Add files via upload

---
+++ src/main/java/com/example/jenkinssonar/JenkinsSonarApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinssonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinssonar/JenkinsSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinssonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
